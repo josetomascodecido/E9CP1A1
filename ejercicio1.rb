19df77fe672ef9cd9a334c55ecdac3699dc04549
Jose Tomas <josetomascodecido@gmail.com>
2018-06-26 20:02:01 -0400

    exercise1

---
+++ ejercicio1.rb
@@ -0,0 +1,23 @@
+## Ejercicio 1: Sintaxis.
+
+# - Copia el siguiente código y ejecútalo. Luego corrige los errores para poder imprimir ejecutar ambos métodos y finalmente obtener el valor de la variable de *a*.
+#
+# ~~~ruby
+class Anything
+  def foo
+    a = 5
+    @a = a
+  end
+
+  def bar
+    @a += 1
+  end
+  def show_a
+    puts @a
+  end
+end
+
+any = Anything.new
+any.foo
+any.bar
+any.show_a
