a1334ad1dc44e4e6598571467f502f49e77cd9a8
Jose Tomas <josetomascodecido@gmail.com>
2018-06-26 20:21:30 -0400

    exercise2

---
+++ ejercicio2.rb
@@ -0,0 +1,24 @@
+## Ejercicio 2: Sintaxis.
+# Corrige el siguiente código para instanciar el objeto y generar la salida de manera correcta.
+#
+# ~~~ruby
+class Car
+  attr_accessor :model, :year
+  def initialize(model, year)
+    @model = model
+    @year = year
+  end
+
+
+end
+
+
+
+
+
+
+
+car = Car.new('Camaro', 2016)
+puts "Mi auto favorito es un #{car.model} del año #{car.year}!"
+car2 = Car.new('Camaro', 2019)
+puts "Mi auto favorito es un #{car2.model} del año #{car2.year}!"
